5b783fa954acf26f52849760a2d5f5dd46ba19d8
John López <johnalbh@gmail.com>
2020-05-02 17:17:08 +0200

    [S1][Implicit Grant]

---
+++ admin/babelrc.js
@@ -0,0 +1,5 @@
+const env = require('./config/setup');
+module.exports = {
+  presets: ['next/babel'],
+  plugins: [['transform-define', env]],
+};
--- blog/index.js
+++ blog/index.js
@@ -1,5 +1,3 @@
-// Importación Libreria
-
 const express = require('express');
 const path = require('path');
 const request = require('request');
@@ -7,10 +5,10 @@ const querystring = require('querystring');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
-//Importación de Utilidad
 const generateRandomString = require('./utils/generateRandomString');
 const encodeBasic = require('./utils/encodeBasic');
-const scopesArray = require('./utils/scopesArrays');
+const scopesArray = require('./utils/scopesArray');
+
 const playlistMocks = require('./utils/mocks/playlist');
 
 const { config } = require('./config');
@@ -20,6 +18,7 @@ const app = express();
 // static files
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
+// middlewares
 app.use(cors());
 app.use(cookieParser());
 
@@ -74,34 +73,56 @@ function getUserPlaylists(accessToken, userId) {
 // routes
 app.get('/', async function (req, res, next) {
   const { access_token: accessToken } = req.cookies;
+
   try {
     const userInfo = await getUserInfo(accessToken);
     res.render('playlists', {
       userInfo,
       isHome: true,
-      playlists: {
-        items: playlistMocks,
-      },
+      playlists: { items: playlistMocks },
     });
   } catch (error) {
     next(error);
   }
 });
+
+app.get('/playlists', async function (req, res, next) {
+  const { access_token: accessToken } = req.cookies;
+
+  if (!accessToken) {
+    return res.redirect('/');
+  }
+
+  try {
+    const userInfo = await getUserInfo(accessToken);
+    const userPlaylists = await getUserPlaylists(accessToken, userInfo.id);
+
+    res.render('playlists', { userInfo, playlists: userPlaylists });
+  } catch (error) {
+    next(error);
+  }
+});
+
 app.get('/login', function (req, res) {
   const state = generateRandomString(16);
 
-  const queryStirng = querystring.stringify({
+  const queryString = querystring.stringify({
     response_type: 'code',
     client_id: config.spotifyClientId,
     scope: scopesArray.join(' '),
     redirect_uri: config.spotifyRedirectUri,
     state: state,
   });
+
   res.cookie('auth_state', state, { httpOnly: true });
-  res.redirect(`https://accounts.spotify.com/authorize?${queryStirng}`);
+  res.redirect(`https://accounts.spotify.com/authorize?${queryString}`);
 });
 
-// Codigo de autorización
+app.get('/logout', function (req, res) {
+  res.clearCookie('access_token');
+  res.redirect('/');
+});
+
 app.get('/callback', function (req, res, next) {
   const { code, state } = req.query;
   const { auth_state } = req.cookies;
@@ -109,7 +130,9 @@ app.get('/callback', function (req, res, next) {
   if (state === null || state !== auth_state) {
     next(new Error("The state doesn't match"));
   }
+
   res.clearCookie('auth_state');
+
   const authOptions = {
     url: 'https://accounts.spotify.com/api/token',
     form: {
@@ -117,7 +140,7 @@ app.get('/callback', function (req, res, next) {
       redirect_uri: config.spotifyRedirectUri,
       grant_type: 'authorization_code',
     },
-    header: {
+    headers: {
       Authorization: `Basic ${encodeBasic(
         config.spotifyClientId,
         config.spotifyClientSecret
@@ -125,12 +148,14 @@ app.get('/callback', function (req, res, next) {
     },
     json: true,
   };
+
   request.post(authOptions, function (error, response, body) {
     if (error || response.statusCode !== 200) {
       next(new Error('The token is invalid'));
     }
+
     res.cookie('access_token', body.access_token, { httpOnly: true });
-    res.redirect('/playlist');
+    res.redirect('/playlists');
   });
 });
 
